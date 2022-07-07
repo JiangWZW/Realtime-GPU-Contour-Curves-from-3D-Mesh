042abb4a21db280b2c483c67ddd03addcd8248f0
JohnBaiShi <2558567540@qq.com>
2022-07-07 20:52:27 +0800

    .
    .

---
+++ gwd_preview_index/googbase_min.js
@@ -0,0 +1,6 @@
+(function(){/*
+
+ Copyright The Closure Library Authors.
+ SPDX-License-Identifier: Apache-2.0
+*/
+(window.goog=window.goog||{}).inherits=function(a,c){function d(){}d.prototype=c.prototype;a.h=c.prototype;a.prototype=new d;a.prototype.constructor=a;a.g=function(f,g,h){for(var e=Array(arguments.length-2),b=2;b<arguments.length;b++)e[b-2]=arguments[b];return c.prototype[g].apply(f,e)}};}).call(this);
